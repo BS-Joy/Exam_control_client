66eab49f1289ec72130c597ed5243891edce4bf6
BS-Joy <badhansamadder5@gmail.com>
2023-07-01 19:30:50 +0600

    changes made on the login page

--- src/components/pages/auth/LogIn.js
+++ src/components/pages/auth/LogIn.js
@@ -5,12 +5,12 @@ import { Slide, ToastContainer, toast } from "react-toastify";
 
 
 const Login = () => {
-  const [passValidation, setPassValidation] = useState(false);
-  const [diuEmail, setDiuEmail] = useState(false);
+  // const [passValidation, setPassValidation] = useState(false);
+  // const [diuEmail, setDiuEmail] = useState(false);
 
   const {logIn} = useContext(AuthContext);
   const redirect = useNavigate();
-  const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*]).{5,}$/;
+  // const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*]).{5,}$/;
 
   const submitHandle = (event) => {
     event.preventDefault()
@@ -19,7 +19,7 @@ const Login = () => {
     const password = event.target.password.value;
 
     logIn(email, password)
-    .then(res => {
+    .then(() => {
       // console.log(res.usr);
       redirect('/home')
     }).catch(err => {
@@ -36,23 +36,23 @@ const Login = () => {
     })
   }
 
-  const checkDiuMail = (event) => {
-    const email = event.target.value;
-    if(email.includes('diu.edu.bd')){
-      setDiuEmail(true);
-    } else {
-      setDiuEmail(false);
-    }
-  }
+  // const checkDiuMail = (event) => {
+  //   const email = event.target.value;
+  //   if(email.includes('diu.edu.bd')){
+  //     setDiuEmail(true);
+  //   } else {
+  //     setDiuEmail(false);
+  //   }
+  // }
 
-  const checkPass = (event) => {
-    const pass = event.target.value;
-    if(passwordRegex.test(pass)){
-      setPassValidation(true)
-    } else {
-      setPassValidation(false)
-    }
-  }
+  // const checkPass = (event) => {
+  //   const pass = event.target.value;
+  //   if(passwordRegex.test(pass)){
+  //     setPassValidation(true)
+  //   } else {
+  //     setPassValidation(false)
+  //   }
+  // }
   return (
     <div className="container mx-auto max-w-screen-xl px-3">
       <div className="max-w-md mx-auto py-6 px-8 mt-20 bg-white rounded shadow-xl">
@@ -78,35 +78,36 @@ const Login = () => {
               Email:
             </label>
             <input
-              onChange={checkDiuMail}
+              // onChange={checkDiuMail}
               type="email"
               name="email"
               id="email"
               placeholder="Email"
               className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600"
             />
-            <p className={`font-bold text-red-600 ${diuEmail ? 'hidden' : 'block'}`}>* Please add diu email</p>
+            {/* <p className={`font-bold text-red-600 ${diuEmail ? 'hidden' : 'block'}`}>* Please add diu email</p> */}
           </div>
           <div>
             <label htmlFor="password" className="block text-gray-800 font-bold">
               Password:
             </label>
             <input
-              onChange={checkPass}
+              // onChange={checkPass}
               type="password"
               name="password"
               id="password"
               placeholder="Password"
               className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600"
             />
-            <div className={`text-sm ${passValidation ? "text-black" : "text-red-600"} mt-3 `}>
+            {/* ${passValidation ? "text-black" : "text-red-600"} */}
+            {/* <div className={`text-sm  mt-3 `}>
               <p className="font-bold">* Password must contain:</p>
               <ul className="list-disc list-inside">
                 <li className="pl-2.5">Minimum length of 5 characters</li>
                 <li className="pl-2.5">At least one capital letter</li>
                 <li className="pl-2.5">At least one special character</li>
               </ul>
-            </div>
+            </div> */}
             <Link
               to="/password_reset"
               className="text-sm font-thin text-gray-800 hover:underline mt-2 inline-block hover:text-indigo-600"
@@ -114,8 +115,8 @@ const Login = () => {
               Forget Password?
             </Link>
           </div>
-
-          <button disabled={ passValidation && diuEmail ? false : true } className="disabled:bg-red-500 disabled:cursor-not-allowed cursor-pointer py-2 px-4 block mt-6 bg-blue-600 hover:bg-blue-700 text-white font-bold w-full text-center rounded">
+          {/* disabled={ passValidation && diuEmail ? false : true } */}
+          <button className="disabled:bg-red-500 disabled:cursor-not-allowed cursor-pointer py-2 px-4 block mt-6 bg-blue-600 hover:bg-blue-700 text-white font-bold w-full text-center rounded">
             Login
           </button>
         </form>
